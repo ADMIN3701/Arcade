989cb5b93516343804d6fad9db927dd43bcb97e5
ADMIN3701 <unknow.ggg.98@gmail.com>
2023-01-19 16:21:23 +0200

    step 3 ( add texture )

--- Main_character.py
+++ Main_character.py
@@ -1,19 +1,20 @@
 import time
 import pygame
 class Charecter():
-    star = pygame.image.load("star_sprite.png")
+    star = pygame.image.load("images/star_sprite.png")
+    star.set_colorkey((255, 255, 255))
     star_x = 0
     star_y = 0
-    curentframe = 0
 
     rect = pygame.Surface((40, 40))
-    rect.fill((0, 255, 255))
-    speed = 0.5
+    rect.fill((255, 255, 255))
+    #rect.set_colorkey((255, 255, 255))
+    speed = 5
     x = 200
     y = 200
 
     def Character_walk(self):
-        self.curentframe += 0000.1
-        if self.curentframe == 10:
-             self.curentframe == 0
-        self.rect.blit(self.star, (self.star_x - self.curentframe, self.star_y))
+        self.rect.blit(self.star, (self.star_x-160, self.star_y))
+        self.star_x += 40
+        if self.star_x >= 160:
+            self.star_x = 0
--- main.py
+++ main.py
@@ -19,19 +19,26 @@ clock = pygame.time.Clock()
 f1 = pygame.font.Font(None, 36)
 text1 = f1.render(str(clock), True, (180, 0, 0))
 
-tile_size = 200
+tile_size = 40
 
 def Lines_on_screen():
     for i in range(0, 250):
         pygame.draw.line(window, (255,255,255), (0, i * tile_size), (full_width, i * tile_size))
         pygame.draw.line(window, (255,255,255), (i * tile_size, 0), (i * tile_size, full_height))
 
-world_data = [[1, 1, 1, 1, 1, 1],
-              [1, 0, 0, 0, 0, 1],
-              [1, 0, 0, 0, 0, 1],
-              [1, 0, 0, 0, 0, 1],
-              [1, 0, 0, 0, 0, 1],
-              [1, 1, 1, 1, 1, 1]
+world_data = [[4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+              [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+              [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+              [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+              [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+              [1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1]
               ]
 
 
@@ -61,19 +68,61 @@ class MAP():
     def __init__(self, world_map):
         self.tile_list = []
 
-        Image = pygame.image.load("wall.png")
+        Background = pygame.image.load("images/background.png")
+        window.blit(Background, (0, 0))
+
+        Ground = pygame.image.load("images/wall.png")
+        sky = pygame.image.load("images/sky.png")
+        sky2 = pygame.image.load("images/sky2.png")
+        sky3 = pygame.image.load("images/sky3.png")
+        tree = pygame.image.load("images/tree.png")
+        tree.set_colorkey((255, 255, 255))
 
         row_count = 0
         for row in world_map:
             col_count = 0
             for col in row:
                 if col == 1:
-                    Transforme_Image = pygame.transform.scale(Image, (tile_size, tile_size))
-                    img_rect = Image.get_rect()
+
+                    Transforme_Image = pygame.transform.scale(Ground, (tile_size, tile_size))
+                    img_rect = Ground.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
-                    tile = (Image, img_rect)
+                    tile = (Ground, img_rect)
                     self.tile_list.append(tile)
+
+                if col == 2:
+                    Transforme_Image = pygame.transform.scale(sky, (tile_size, tile_size))
+                    img_rect = sky.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (sky, img_rect)
+                    self.tile_list.append(tile)
+
+                if col == 3:
+                    Transforme_Image = pygame.transform.scale(sky2, (tile_size, tile_size))
+                    img_rect = sky2.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (sky2, img_rect)
+                    self.tile_list.append(tile)
+
+                if col == 4:
+                    Transforme_Image = pygame.transform.scale(sky3, (tile_size, tile_size))
+                    img_rect = sky3.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (sky3, img_rect)
+                    self.tile_list.append(tile)
+
+                if col == 5:
+                    Transforme_Image = pygame.transform.scale(tree, (tile_size, tile_size))
+                    img_rect = tree.get_rect()
+                    img_rect.x = col_count * tile_size - 100
+                    img_rect.y = row_count * tile_size - 180
+                    tile = (tree, img_rect)
+                    self.tile_list.append(tile)
+
                 col_count += 1
             row_count += 1
     def draw(self):
@@ -89,7 +138,8 @@ done = True
 while done:
 
     World.draw()
-    Character.Character_walk()
+    #Character.Character_walk()
+    window.blit(Character.rect, (Character.x, Character.y))
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
@@ -107,3 +157,4 @@ while done:
     pygame.display.flip()
 
 pygame.quit()
+
